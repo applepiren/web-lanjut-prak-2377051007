702a7a912fb95aa887109c6f7225420e5e13ead2
Fiorentina Aamara Putri Emile <143419596+applepiren@users.noreply.github.com>
2025-04-23 12:39:31 +0700

    uploud file

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'foto'
     ];
 
     /**
@@ -42,6 +43,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    
 }
